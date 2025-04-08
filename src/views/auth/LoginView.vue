968cb576aa8bafb251bb67d4c48d148a848b2db0
charles <charlesneri0201@gmail.com>
2025-04-08 23:57:04 +0800

    With register and Login

---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar class="px-3">
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text=""
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container
+          fluid
+          style="min-height: 100vh; background: linear-gradient(140deg, #3b939a, #68cad5)"
+        >
+          <v-row>
+            <v-col cols="12" md="6" class="mx-auto">
+              <v-card class="mx-auto rounded-xl pb-5" width="500" style="font-size: 85%" elevation="20 ">
+                <v-card-title class="text-h5">Login</v-card-title>
+
+                <template v-slot:title>
+                  <v-img
+                    src="/nb_logo.png"
+                    :width="100"
+                    class="mx-auto"
+                    aspect-ratio="16/9"
+                    cover
+                  ></v-img>
+                  <span class="font-weight-black d-flex justify-center pt-3">Teach & Learn</span>
+                </template>
+
+                <v-card-text class="p-4">
+                  <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Email" variant="outlined"></v-text-field>
+
+                    <v-text-field
+                      label="Password"
+                      type="password"
+                      variant="outlined"
+                    ></v-text-field>
+
+                    <v-btn
+                      color="light-blue-darken-2"
+                      class="mt-2"
+                      type="submit"
+                      prepend-icon="mdi-login"
+                      block
+                    >
+                      Login
+                    </v-btn>
+
+                    <v-divider class="my-5"></v-divider>
+                    <p class="text-center text-primary">
+                      Don't account yet? <RouterLink to="/register">Register now</RouterLink>
+                    </p>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
---
+++ src/views/auth/RegisterView.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar class="px-3">
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text=""
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container
+          fluid
+          style="min-height: 100vh; background: linear-gradient(140deg, #3b939a, #68cad5)"
+        >
+          <v-row>
+            <v-col cols="12" md="6" class="mx-auto">
+              <v-card class="mx-auto rounded-xl pb-5" width="500">
+                <template v-slot:title>
+                  <span class="font-weight-black d-flex justify-center">Register Now!</span>
+                </template>
+
+                <v-card-text class="bg-surface-light pt-4">
+                  <v-sheet class="mx-auto" width="300">
+                    <v-form fast-fail @submit.prevent>
+                      <v-text-field label="first name" variant="outlined"></v-text-field>
+                      <v-text-field label="Last name" variant="outlined"></v-text-field>
+                      <v-text-field label="Gender" variant="outlined"></v-text-field>
+                      <v-text-field label="Email" variant="outlined"></v-text-field>
+                      <v-text-field label="Email" variant="outlined"></v-text-field>
+
+                      <v-text-field
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                      ></v-text-field>
+                      <v-text-field
+                        label="Confirm Password"
+                        type="password"
+                        variant="outlined"
+                      ></v-text-field>
+
+                      <v-btn
+                        color="light-blue-darken-2"
+                        class="mt-2"
+                        type="submit"
+                        prepend-icon="mdi-account-plus"
+                        block
+                      >
+                        Signup
+                      </v-btn>
+                      
+                      <v-divider class="my-5"></v-divider>
+                      <p class="text-center text-primary">
+                        Already have account? <RouterLink to="/login"> Login Now!</RouterLink>
+                      </p>
+                    </v-form>
+                  </v-sheet>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
