627fd2788bda624e3545530a309a912c610c7303
charles <charlesneri0201@gmail.com>
2025-04-13 21:14:24 +0800

    updating homeview.vue

--- src/views/Layout/HomeView.vue
+++ src/views/Layout/HomeView.vue
@@ -5,12 +5,12 @@ const links = ['Mentor', 'About us', 'Contact']
 <template>
   <v-app id="inspire">
     <!-- App Bar -->
-    <v-app-bar flat>
+    <v-app-bar flat color="#1565c0">
       <v-container class="d-flex align-center justify-space-between">
         <!-- Left: Logo + Menu -->
-        <div class="d-flex align-center gap-2">
+        <div class="d-flex align-center gap-5">
           <!-- Logo Avatar -->
-          <v-avatar style="border-width: 1px; color: #000" size="32">
+          <v-avatar style="border-width: 1px" color="#fff" size="50">
             <v-img src="image/Teach&Learn.png" alt="Logo" />
           </v-avatar>
 
@@ -23,7 +23,7 @@ const links = ['Mentor', 'About us', 'Contact']
             </template>
 
             <v-list>
-              <v-list-item link title="My attict Profile"></v-list-item>
+              <v-list-item link title="My Profile"></v-list-item>
               <v-list-item link title="My Appointment"></v-list-item>
               <v-divider></v-divider>
               <v-list-item link> <router-link to="/">Logout</router-link></v-list-item>
@@ -32,7 +32,7 @@ const links = ['Mentor', 'About us', 'Contact']
         </div>
 
         <!-- Center: Page Links -->
-        <div class="d-flex align-center gap-2">
+        <div class="profile-container d-flex align-center gap-2">
           <v-btn
             v-for="link in links"
             :key="link"
@@ -55,7 +55,7 @@ const links = ['Mentor', 'About us', 'Contact']
               single-line
               class="flex-grow-1"
             ></v-text-field>
-            <v-btn color="light-blue-darken-2" icon rounded="lg" class="elevation-1">
+            <v-btn color="#fff" icon rounded="lg" class="elevation-1" style="border: 10px">
               <v-icon>mdi-magnify</v-icon>
             </v-btn>
           </div>
@@ -65,31 +65,30 @@ const links = ['Mentor', 'About us', 'Contact']
 
     <!-- Main Content -->
     <v-main class="bg-grey-lighten-3">
-      <v-container>
+      <v-container fluid class="pa-0" style="max-width: 1400px; margin: auto">
         <v-row>
-          <!-- Optional Left Column -->
-          <v-col cols="2">
-            <v-sheet rounded="lg" class="pa-4 text-center">
-              <v-avatar class="mx-auto mb-2" style="border-width: 1px; color: #000" size="50">
+          <!-- Wider Left Column -->
+          <v-col cols="3">
+            <v-sheet rounded="lg" class="pa-4 text-center" style="width: 100%; height: 100%">
+              <v-avatar class="mb-3" style="border-width: 1px; color: #000" size="100">
                 <v-img src="image/Teach&Learn.png" alt="User" />
               </v-avatar>
-              <v-list rounded="lg" density="compact">
-                <v-list-item link title="My Profile"></v-list-item>
-                <v-list-item link title="My Appointment"></v-list-item>
-                <v-divider class="my-2"></v-divider>
-                <v-list-item link title="Logout"></v-list-item>
-              </v-list>
+              <span class="d-block font-weight-medium mx-auto">My Name</span>
+              <!-- Full-width Divider -->
+              <v-divider class="ma-0 pa-0" style="width: 100%; display: block"></v-divider>
+              <span class="text-left d-block"><b>My Appointments</b></span>
             </v-sheet>
           </v-col>
 
-          <!-- Main Section -->
-          <v-col>
-            <v-sheet min-height="70vh" rounded="lg" class="pa-4">
+          <!-- Main Content Column -->
+          <v-col cols="9">
+            <v-sheet min-height="70vh" rounded="lg" class="pa-4" style="width: 100%">
               <!-- Your content goes here -->
             </v-sheet>
           </v-col>
         </v-row>
       </v-container>
     </v-main>
+    <v-footer></v-footer>
   </v-app>
 </template>
