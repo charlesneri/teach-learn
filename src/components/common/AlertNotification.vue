6aa7249d1b7ed79c4d0267b76307e089fdb68181
Charles <charlesneri0201@gmail.com>
2025-05-02 20:34:46 +0800

    feat: all done feature, need testing

--- src/components/common/AlertNotification.vue
+++ src/components/common/AlertNotification.vue
@@ -1,29 +1,59 @@
 <script setup>
+import { ref, watch } from 'vue'
+
 const props = defineProps(['formSuccessMessage', 'formErrorMessage'])
+
+const showSuccess = ref(false)
+const showError = ref(false)
+
+// Auto-dismiss logic
+watch(
+  () => props.formSuccessMessage,
+  (msg) => {
+    if (msg) {
+      showSuccess.value = true
+      setTimeout(() => {
+        showSuccess.value = false
+      }, 4000) // auto-hide after 4s
+    }
+  }
+)
+
+watch(
+  () => props.formErrorMessage,
+  (msg) => {
+    if (msg) {
+      showError.value = true
+      setTimeout(() => {
+        showError.value = false
+      }, 2000) 
+    }
+  }
+)
 </script>
 
 <template>
-  <!--Alert-->
   <v-alert
-    v-if="props.formSuccessMessage"
+    v-if="showSuccess"
     :text="props.formSuccessMessage"
-    title="success"
+    title="Success"
     type="success"
-    variant="submit"
-    density="compact"
-    border="start"
-    closable
-  >
-  </v-alert>
-  <v-alert
-    v-if="props.formErrorMessage"
-    :text="props.formErrorMessage"
-    title="Ooopps!"
     variant="tonal"
-    type="error"
     density="compact"
     border="start"
     closable
-  >
-  </v-alert>
+    @click:close="showSuccess = false"
+  />
+
+  <v-alert
+    v-if="showError"
+    :text="props.formErrorMessage"
+    title="Oops!"
+    type="error"
+    variant="tonal"
+    density="compact"
+    border="start"
+    closable
+    @click:close="showError = false"
+  />
 </template>
